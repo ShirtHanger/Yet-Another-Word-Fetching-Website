7fa57891c655d12feae6530ed467649d1a82dcf6
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2025-05-27 16:34:30 -0500

    Crunched event listener into multiple main and helper functions

---
+++ functions.js
@@ -0,0 +1,16 @@
+import * as elements from "./script.js"
+
+
+
+// export {
+//     randNum,
+//     renderSearchResultTitle,
+//     appendAltWordsToScreen,
+//     appendMeanings,
+//     renderContainerBase,
+//     renderResponse,
+//     renderFullWordData,
+//     renderSourceList,
+//     clearScreen,
+//     getWord
+// }
--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 import { apiLink } from "./link.js"
 
+// import * as functions from "./js"
+
 const userInput = document.getElementById("user-input")
 const submitButton = document.getElementById("submit-button")
 
 const fetchedWordContainer = document.getElementById("fetched-word-container")
 
 const inputDisplay = document.getElementById("input-display")
-
-const partOfSpeechDisplay = document.getElementById("part-of-speech-display")
-const definitionList = document.getElementById("definition-list")
-
-const exampleList = document.getElementById("example-list")
-const synonymsDisplay = document.getElementById("synonyms-display")
-const antonymsDisplay = document.getElementById("antonyms-display")
 
 const sourceTitle = document.getElementById("source-title")
 const sourceList = document.getElementById("source-list")
@@ -22,119 +17,76 @@ const sourceList = document.getElementById("source-list")
 submitButton.addEventListener("click", async () => {
 
     /* Cleans the screen up */
-    // clearScreen()
-    clearScreenButBetter(fetchedWordContainer, sourceList, inputDisplay, userInput)
+    clearScreen(fetchedWordContainer, sourceList, inputDisplay, userInput)
 
     let fetchedWord = userInput.value.trim()
 
     let wordData = await getWord(apiLink, fetchedWord) /* Returns first index in response */
 
-    /* This will place the word to the screen */
-    /* If no phonetic, print just the word only. */
-    if (!wordData.phonetic) {
-        inputDisplay.innerHTML = `${wordData.word}`
-    } else {
-    /* If there is a word and it has a phonetic, proceed as normal */
-        inputDisplay.innerHTML = `${wordData.word} (${wordData.phonetic})`
-    }
+    renderSearchResultTitle(wordData)
 
-    /* No error handling as of now */
+    renderResponse(wordData)
+
+
+    renderSourceList(wordData)
+
+})
+
+/* LARGE FUNCTIONS */
+
+/* Function to create */
+function renderResponse(wordData) {
 
     /* wordData.meanings the drill that contains everything */
 
-    fetchedWordContainer.appendChild(inputDisplay)
-
-    
     for (let aspect of wordData.meanings) {
-
 
         let responseBlock = document.createElement("article")
         responseBlock.classList.add("response-block")
 
-        responseBlock.innerHTML = `
-            <h3>As a ${aspect.partOfSpeech}</h3>
-            <h4>Definitions</h4>
-            <ul class="definition-list" id="${aspect.partOfSpeech}-definition"></ul>
-            <h4>Examples</h4>
-            <ul class="example-list" id="${aspect.partOfSpeech}-example"></ul>
 
-
-            <section id="synonyms-antonyms-block">
-
-                <article class="synonyms-display">
-                    <h5>Synonyms</h5>
-                    <ul class="synonyms-list" id="${aspect.partOfSpeech}-synonym"></ul>
-                </article>
-
-                <article class="antonyms-display">
-                    <h5>Antonyms</h5>
-                    <ul class="antonyms-list" id="${aspect.partOfSpeech}-antonym"></ul>
-                </article>
-
-            </section>
-        `
-        
+        /* Create article element for data population, renders it */
+        let newContainer = renderContainerBase(aspect.partOfSpeech)
+        responseBlock.innerHTML = newContainer
         fetchedWordContainer.appendChild(responseBlock)
 
-        /* Redefines each element, with the part of speech attached, so the loop properly targets each part of speech */
+        /* Redefine each element, with the part of speech attached, so the loop properly targets each part of speech */
         let definitionList = document.getElementById(`${aspect.partOfSpeech}-definition`)
         let exampleList = document.getElementById(`${aspect.partOfSpeech}-example`)
+        let synonymList = document.getElementById(`${aspect.partOfSpeech}-synonym`)
+        let antonymList = document.getElementById(`${aspect.partOfSpeech}-antonym`)
 
         /* Work on variable names to make more readable */
 
+        renderFullWordData(aspect, definitionList, exampleList, synonymList, antonymList)
+        /* Render all definitions, examples, synonyms, and antonyms to the screen */
 
-        /* Appends each definition to the list of definitions, cannot crunch into function for some reason. */
+    }
+}
+
+/* Maps all word information to the page in proper HTML elements */
+function renderFullWordData(aspect, definitionList, exampleList, synonymList, antonymList) {
+        // Render all defintions on screen
+
+        for (let item of aspect.definitions) {  
+            appendMeanings(item.definition, definitionList)
+        }
+
+        // Render all examples on screen
         for (let item of aspect.definitions) {
-            // console.log(item)
-            let newDefinition = document.createElement("li")
-            newDefinition.innerHTML = `${item.definition}`
-            definitionList.appendChild(newDefinition)
-            
+            appendMeanings(item.example, exampleList)
         }
 
-        /* Appends each example to the list of examples, cannot crunch into function for some reason. */
+        // Render all synonyms and antonyms on screen
+        appendAltWordsToScreen(aspect.synonyms, synonymList)
+        appendAltWordsToScreen(aspect.antonyms, antonymList)
 
-        for (let item of aspect.definitions) {
-            let newExample = document.createElement("li")
-            if (item.example) {
-                newExample.innerHTML = `${item.example}`
-                exampleList.appendChild(newExample)
-            }
-        }
-
-        /* Appends each synonym to the list of synonyms, cannot crunch into function for some reason. */
-        if (aspect.synonyms.length > 0) {
-            let synonymList = document.getElementById(`${aspect.partOfSpeech}-synonym`)
-            for (let item of aspect.synonyms) {
-                let newSynonym = document.createElement("li")
-                newSynonym.innerHTML = `${item}`
-                synonymList.appendChild(newSynonym)
-            }
-        }
-
-        /* Appends each antonym to the list of antonyms, cannot crunch into function for some reason. */
-        if (aspect.antonyms.length > 0) {
-            let antonymList = document.getElementById(`${aspect.partOfSpeech}-antonym`)
-            for (let item of aspect.antonyms) {
-                let newAntonym = document.createElement("li")
-                newAntonym.innerHTML = `${item}`
-                antonymList.appendChild(newAntonym)
-            }
-        }
-
+        // Here to let me know the loop was successful
         console.log(`completed iteration of ${aspect.partOfSpeech}`)
 
-    }
+}
 
-    for (let source of wordData.sourceUrls) {
-        let newSource = document.createElement("li")
-        newSource.innerHTML = `<a href="${source}">${source}</a>`
-        sourceList.appendChild(newSource)
-    }
-
-})
-
-/* FUNCTIONS */
+/* HELPER FUNCTIONS */
 
 function randNum(maxNum) {
     /* Returns a random number between 0 and the length of given array */
@@ -144,35 +96,97 @@ function randNum(maxNum) {
     return randIndex
 }
 
-function appendDefinitionToScreen(item, newDefinition) {
-        
-    /* Write a function to call each time the loop for adding part of speech and definitions for a word iterates */
+/* Renders the user input above the API data pull */
+function renderSearchResultTitle(apiData) {
+
+    /* If no phonetic, display the word only. */
+    if (!apiData.phonetic) {
+        inputDisplay.innerHTML = `${wordData.word}`
+    } else {
+    /* If word has a phonetic, proceed as normal */
+        inputDisplay.innerHTML = `${apiData.word} (${apiData.phonetic})`
+    }
+
+    fetchedWordContainer.appendChild(inputDisplay)
 }
 
-function appendExampleToScreen(item, newExample) {
-            
-    /* Write a function to call each time the loop for adding part of speech and definitions for a word iterates */
+/* Maps synonyms and antonyms to the site */
+function appendAltWordsToScreen(altWordsArray, altWordListEl) {
+
+    if (altWordsArray.length > 0) {
+        for (let altWord of altWordsArray) {
+            let newAltWordEl = document.createElement("li")
+            newAltWordEl.innerHTML = `${altWord}`
+            altWordListEl.appendChild(newAltWordEl)
+        }
+    }
+    
 }
 
-function clearScreen() {
-    /* Cleans up previous API pull */
-    fetchedWordContainer.innerHTML = ""
-    sourceList.innerHTML = ""
-    inputDisplay.innerHTML = ""
+/* Maps definitions/examples to the site */
+function appendMeanings(meaning, meaningList) {
+
+        if (meaning) {
+            let NewMeaning = document.createElement("li")
+            NewMeaning.innerHTML = `${meaning}`
+            meaningList.appendChild(NewMeaning)
+        }
 }
 
-function clearScreenButBetter(...elements) {
+
+/* Creates the base article element used to auto populate API pull */
+function renderContainerBase(partOfSpeech) {
+
+        let containerBase = `
+            <h3>As a ${partOfSpeech}</h3>
+            <h4>Definitions</h4>
+            <ul class="definition-list" id="${partOfSpeech}-definition"></ul>
+            <h4>Examples</h4>
+            <ul class="example-list" id="${partOfSpeech}-example"></ul>
+
+
+            <section id="synonyms-antonyms-block">
+
+                <article class="synonyms-display">
+                    <h5>Synonyms</h5>
+                    <ul class="synonyms-list" id="${partOfSpeech}-synonym"></ul>
+                </article>
+
+                <article class="antonyms-display">
+                    <h5>Antonyms</h5>
+                    <ul class="antonyms-list" id="${partOfSpeech}-antonym"></ul>
+                </article>
+
+            </section>
+        `
+
+        return containerBase
+}
+
+function renderSourceList(wordData) {
+    for (let source of wordData.sourceUrls) {
+        let newSource = document.createElement("li")
+        newSource.innerHTML = `<a href="${source}">${source}</a>`
+        sourceList.appendChild(newSource)
+    }
+}
+
+/* Accepts any number of arguements, clears the API data of all DOM elements passed into it */
+function clearScreen(...elements) {
     for (let element of elements) {
         element.innerHTML = ""
     }
 }
 
-/* AXIOS FUNCTIONS */
+/* AXIOS API FUNCTIONS */
 
+/* Literally just grabs the word from the API */
 async function getWord(drill, word) {
+
     let response = await axios.get(`${drill}${word}`)
     // console.log(response.data)
     return response.data[0]
     /* Returns a list containing 1 or multiple objects */
     /* Literally all we need is the first item. IDK why the API is set up like this */
+
 }
